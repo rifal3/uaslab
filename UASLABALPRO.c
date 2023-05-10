219d49a3c2641a39bbc5fdb89ad06eb63457ef2e
SugengL <133039748+SugengL@users.noreply.github.com>
2023-05-10 20:37:07 +0700

    Update UASLABALPRO.c

--- UASLABALPRO.c
+++ UASLABALPRO.c
@@ -69,3 +69,105 @@ int main() {
     return 0;
 }
 
+// fungsi untuk membersihkan input buffer
+void clearInputBuffer() {
+    int c;
+    while (( c = getchar()) !='\n' && c != EOF);
+}
+
+//fungsi untuk menambahkan buku kedalam daftar buku
+void tambahBuku() {
+    printf("Masukkan judul buku: ");
+    scanf("%[^\n]", daftarBuku[JumlahBuku].judul);
+    clearInputBuffer();
+
+    printf("Masukkan nama pengarang: ");
+    scanf("%[^\n]", daftarBuku[JumlahBuku].pengarang);
+    clearInputBuffer();
+    
+    printf("Masukkan tahun terbit: ");
+    scanf("%d", daftarBuku[JumlahBuku].tahunTerbit);
+    clearInputBuffer();
+
+    printf("Masukkan penerbit: ");
+    scanf("%[^\n]", daftarBuku[JumlahBuku].penerbit);
+    clearInputBuffer();
+
+    daftarBuku[jumlahBuku].status = 0;
+
+    jumlahbuku++;
+
+    printf("Buku berhasil ditambahkan. \n\n");
+}
+
+
+// fungsi untuk menampilkan data semua buku
+void tampilkanDaftarBuku() {
+    if (jumlahBuku == 0) {
+        printf("Belum ada buku yang ditambahkan. \n\n");
+    } else {
+        printf("Data buku:\n");
+        for (int i = 0; i < jumlahBuku; i++) {
+            printf("Judul buku: %s\n", daftarBuku[i].judul);
+            printf("Pengarang: %s\n", daftarBuku[i].pengarang);
+            printf("Tahun terbit: %d\n", daftarBuku[i].tahunterbit);
+            printf("Penerbit: %s\n", daftarBuku[i].penerbit);
+            if (daftarBuku[i].status == 0) {
+                printf("Status: Tersedia\n\n");
+            } else {
+                printf("Status: Dipinjam\n\n");
+            }
+        }
+    }
+}
+
+// fungsi untuk mencari buku berdasarkan judul
+void cariBuku() {
+    char judulCari[100];
+    int found = 0;
+
+    printf("Masukkan judul buku yang ingin dicari: ");
+    scanf("%[^\n]", judulCari);
+    clearInputBuffer();
+    
+    for (int i = 0; i < jumlahBuku; i++) {
+        if (strcmp(judulCari, daftarBuku[i].judul) == 0) {
+            printf("Judul buku: %s\n", daftarBuku[i].judul);
+            printf("Pengarang: %s\n", daftarBuku[i].pengarang);
+        printf("Tahun terbit: %d\n", daftarBuku[i].tahunTerbit);
+        printf("Penerbit:%s\n\n",daftarBuku[i].penerbit);
+        found = 1;
+        break;
+        }
+    }
+    
+    if (!found) {
+        printf("Buku tidak ditemukan.\n\n");
+    }
+}
+
+void pinjamBuku() {
+    char judulPinjam[100];
+    intfound = 0;
+    
+    printf("Masukkan judul buku yang ingin dipinjam: ");
+    scanf("%[^\n]", judulPinjam);
+    clearInputBuffer();
+    
+    for (int i = 0; i < jumlahBuku; i++) {
+        if (strcmp(judulPinjam, daftarBuku[i].judul) == 0) {
+            if (daftarBuku[i].status == 0) {
+                printf("Buku berhasil dipinjam.\n\n");
+                daftarBuku[i].status = 1;
+            } else {
+                printf("Maaf, buku sedang dipinjam.\n\n");
+            }
+            found = 1;
+            break;
+        }
+    }
+    
+    if (!found) {
+        printf("Buku tidak ditemukan.\n\n");
+    }
+}
